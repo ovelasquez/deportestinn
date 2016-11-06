6f27aed9960149a11c9fcd007fa3596d90c88ca1
Oscar Velasquez <velasquez.oscar@gmail.com>
2016-11-06 09:20:06 -0430

    Integracion de todos los datos de los atletas en la clase Atletas

--- src/BackendBundle/Entity/Atletas.php
+++ src/BackendBundle/Entity/Atletas.php
@@ -10,8 +10,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="atletas")
  * @ORM\Entity
  */
-class Atletas
-{
+class Atletas {
+
     /**
      * @var integer
      *
@@ -112,6 +112,130 @@ class Atletas
      */
     private $status;
 
+    //DATOS LABORALES
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="institucion", type="string", length=255, nullable=false)
+     */
+    private $institucion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="departamento", type="string", length=255, nullable=false)
+     */
+    private $departamento;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contancia", type="string", length=255, nullable=false)
+     */
+    private $contancia;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="carnet", type="string", length=255, nullable=false)
+     */
+    private $carnet;
+    
+    // DATOS MEDICOS
+    
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="altura", type="decimal", precision=10, scale=0, nullable=false)
+     */
+    private $altura;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="peso", type="decimal", precision=10, scale=0, nullable=false)
+     */
+    private $peso;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tipo_sangre", type="string", length=10, nullable=false)
+     */
+    private $tipoSangre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alergias", type="text", nullable=false)
+     */
+    private $alergias;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contacto_nombre", type="string", length=255, nullable=false)
+     */
+    private $contactoNombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contacto_telefono", type="string", length=255, nullable=false)
+     */
+    private $contactoTelefono;
+    
+    // DATOS UNIFORMES
+    
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_franela", type="string", length=10, nullable=false)
+     */
+    private $tallaFranela;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_pantalon", type="string", length=10, nullable=false)
+     */
+    private $tallaPantalon;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_pantalon_corto", type="string", length=10, nullable=false)
+     */
+    private $tallaPantalonCorto;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_zapato", type="string", length=10, nullable=false)
+     */
+    private $tallaZapato;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_gorra", type="string", length=10, nullable=false)
+     */
+    private $tallaGorra;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_chaqueta", type="string", length=10, nullable=false)
+     */
+    private $tallaChaqueta;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="talla_medias", type="string", length=10, nullable=false)
+     */
+    private $tallaMedias;
 
 
     /**
@@ -434,5 +558,417 @@ class Atletas
     public function getStatus()
     {
         return $this->status;
+    }
+
+    /**
+     * Set institucion
+     *
+     * @param string $institucion
+     *
+     * @return Atletas
+     */
+    public function setInstitucion($institucion)
+    {
+        $this->institucion = $institucion;
+
+        return $this;
+    }
+
+    /**
+     * Get institucion
+     *
+     * @return string
+     */
+    public function getInstitucion()
+    {
+        return $this->institucion;
+    }
+
+    /**
+     * Set departamento
+     *
+     * @param string $departamento
+     *
+     * @return Atletas
+     */
+    public function setDepartamento($departamento)
+    {
+        $this->departamento = $departamento;
+
+        return $this;
+    }
+
+    /**
+     * Get departamento
+     *
+     * @return string
+     */
+    public function getDepartamento()
+    {
+        return $this->departamento;
+    }
+
+    /**
+     * Set contancia
+     *
+     * @param string $contancia
+     *
+     * @return Atletas
+     */
+    public function setContancia($contancia)
+    {
+        $this->contancia = $contancia;
+
+        return $this;
+    }
+
+    /**
+     * Get contancia
+     *
+     * @return string
+     */
+    public function getContancia()
+    {
+        return $this->contancia;
+    }
+
+    /**
+     * Set carnet
+     *
+     * @param string $carnet
+     *
+     * @return Atletas
+     */
+    public function setCarnet($carnet)
+    {
+        $this->carnet = $carnet;
+
+        return $this;
+    }
+
+    /**
+     * Get carnet
+     *
+     * @return string
+     */
+    public function getCarnet()
+    {
+        return $this->carnet;
+    }
+
+    /**
+     * Set altura
+     *
+     * @param string $altura
+     *
+     * @return Atletas
+     */
+    public function setAltura($altura)
+    {
+        $this->altura = $altura;
+
+        return $this;
+    }
+
+    /**
+     * Get altura
+     *
+     * @return string
+     */
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+
+    /**
+     * Set peso
+     *
+     * @param string $peso
+     *
+     * @return Atletas
+     */
+    public function setPeso($peso)
+    {
+        $this->peso = $peso;
+
+        return $this;
+    }
+
+    /**
+     * Get peso
+     *
+     * @return string
+     */
+    public function getPeso()
+    {
+        return $this->peso;
+    }
+
+    /**
+     * Set tipoSangre
+     *
+     * @param string $tipoSangre
+     *
+     * @return Atletas
+     */
+    public function setTipoSangre($tipoSangre)
+    {
+        $this->tipoSangre = $tipoSangre;
+
+        return $this;
+    }
+
+    /**
+     * Get tipoSangre
+     *
+     * @return string
+     */
+    public function getTipoSangre()
+    {
+        return $this->tipoSangre;
+    }
+
+    /**
+     * Set alergias
+     *
+     * @param string $alergias
+     *
+     * @return Atletas
+     */
+    public function setAlergias($alergias)
+    {
+        $this->alergias = $alergias;
+
+        return $this;
+    }
+
+    /**
+     * Get alergias
+     *
+     * @return string
+     */
+    public function getAlergias()
+    {
+        return $this->alergias;
+    }
+
+    /**
+     * Set contactoNombre
+     *
+     * @param string $contactoNombre
+     *
+     * @return Atletas
+     */
+    public function setContactoNombre($contactoNombre)
+    {
+        $this->contactoNombre = $contactoNombre;
+
+        return $this;
+    }
+
+    /**
+     * Get contactoNombre
+     *
+     * @return string
+     */
+    public function getContactoNombre()
+    {
+        return $this->contactoNombre;
+    }
+
+    /**
+     * Set contactoTelefono
+     *
+     * @param string $contactoTelefono
+     *
+     * @return Atletas
+     */
+    public function setContactoTelefono($contactoTelefono)
+    {
+        $this->contactoTelefono = $contactoTelefono;
+
+        return $this;
+    }
+
+    /**
+     * Get contactoTelefono
+     *
+     * @return string
+     */
+    public function getContactoTelefono()
+    {
+        return $this->contactoTelefono;
+    }
+
+    /**
+     * Set tallaFranela
+     *
+     * @param string $tallaFranela
+     *
+     * @return Atletas
+     */
+    public function setTallaFranela($tallaFranela)
+    {
+        $this->tallaFranela = $tallaFranela;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaFranela
+     *
+     * @return string
+     */
+    public function getTallaFranela()
+    {
+        return $this->tallaFranela;
+    }
+
+    /**
+     * Set tallaPantalon
+     *
+     * @param string $tallaPantalon
+     *
+     * @return Atletas
+     */
+    public function setTallaPantalon($tallaPantalon)
+    {
+        $this->tallaPantalon = $tallaPantalon;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaPantalon
+     *
+     * @return string
+     */
+    public function getTallaPantalon()
+    {
+        return $this->tallaPantalon;
+    }
+
+    /**
+     * Set tallaPantalonCorto
+     *
+     * @param string $tallaPantalonCorto
+     *
+     * @return Atletas
+     */
+    public function setTallaPantalonCorto($tallaPantalonCorto)
+    {
+        $this->tallaPantalonCorto = $tallaPantalonCorto;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaPantalonCorto
+     *
+     * @return string
+     */
+    public function getTallaPantalonCorto()
+    {
+        return $this->tallaPantalonCorto;
+    }
+
+    /**
+     * Set tallaZapato
+     *
+     * @param string $tallaZapato
+     *
+     * @return Atletas
+     */
+    public function setTallaZapato($tallaZapato)
+    {
+        $this->tallaZapato = $tallaZapato;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaZapato
+     *
+     * @return string
+     */
+    public function getTallaZapato()
+    {
+        return $this->tallaZapato;
+    }
+
+    /**
+     * Set tallaGorra
+     *
+     * @param string $tallaGorra
+     *
+     * @return Atletas
+     */
+    public function setTallaGorra($tallaGorra)
+    {
+        $this->tallaGorra = $tallaGorra;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaGorra
+     *
+     * @return string
+     */
+    public function getTallaGorra()
+    {
+        return $this->tallaGorra;
+    }
+
+    /**
+     * Set tallaChaqueta
+     *
+     * @param string $tallaChaqueta
+     *
+     * @return Atletas
+     */
+    public function setTallaChaqueta($tallaChaqueta)
+    {
+        $this->tallaChaqueta = $tallaChaqueta;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaChaqueta
+     *
+     * @return string
+     */
+    public function getTallaChaqueta()
+    {
+        return $this->tallaChaqueta;
+    }
+
+    /**
+     * Set tallaMedias
+     *
+     * @param string $tallaMedias
+     *
+     * @return Atletas
+     */
+    public function setTallaMedias($tallaMedias)
+    {
+        $this->tallaMedias = $tallaMedias;
+
+        return $this;
+    }
+
+    /**
+     * Get tallaMedias
+     *
+     * @return string
+     */
+    public function getTallaMedias()
+    {
+        return $this->tallaMedias;
+    }
+    
+    public function __toString() {
+        return $this->getCedula();
     }
 }
